ef9c4d461ca415a544e7ff526ed55d7d4f86d081
Kerin Wong <kerinwong97@gmail.com>
2024-03-20 17:10:27 +0800

    header link

--- src/common/SecureEatsHeader.tsx
+++ src/common/SecureEatsHeader.tsx
@@ -1,7 +1,18 @@
 import { Button, HStack, Heading } from "native-base";
+import { useNavigate } from "react-router-dom";
 
 export default function SecureEatsHeader()
 {
+    const navigate = useNavigate();
+
+    const goToProfile = () => {
+        navigate("/secure-eats/profile"); // Navigate to the ProfilePage
+    };
+
+    const goToCustomerReview = () => {
+        navigate("/secure-eats/customer-review"); // Navigate to the ProfilePage
+    };
+    
     return (
         <HStack p = {5} alignItems="center" display="flex" justifyContent="space-between" backgroundColor="#FFA629">
             <Heading size='xl' color = "white">SecureEats</Heading>
@@ -9,10 +20,10 @@ export default function SecureEatsHeader()
                 <Button variant = "link" size="lg" _text={{color: "white"}}>
                     Orders
                 </Button>
-                <Button variant = "link" size="lg" _text={{color: "white"}}>
+                <Button variant = "link" size="lg" _text={{color: "white"}} onPress={goToCustomerReview}>
                     Review
                 </Button>
-                <Button variant = "link" size="lg" _text={{color: "white"}}>
+                <Button variant = "link" size="lg" _text={{color: "white"}} onPress={goToProfile}>
                     Profile
                 </Button>
             </HStack>
